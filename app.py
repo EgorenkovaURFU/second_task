dbaf510d92701b863f417da36a6a735fb0940880
Tanya <tatyana-baeva@list.ru>
2023-11-20 15:06:42 +0700

    cache problem

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-import io
-import streamlit as st
-from PIL import Image
 from tensorflow.keras.applications import EfficientNetB0
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.efficientnet import preprocess_input
 from tensorflow.keras.applications.efficientnet import decode_predictions
 import numpy as np
+import io
+import streamlit as st
+from PIL import Image
 
 
+@st.cache_data
 def load_image():
     """Создание формы для загрузки изображения"""
     # Форма для загрузки изображения средствами Streamlit
